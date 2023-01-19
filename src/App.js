abd292903586145a5609a1b35e40b0a03771c331
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2023-01-19 22:35:57 +0900

    home set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Title from './component/Title';
+import FirstList from './pages/FirstList';
 
 function App() {
   return (
-    <div className="App">
-      yoon
-    </div>
+    <Compoenet>
+      <HashRouter>
+      
+        <Nav>
+          <Title></Title>
+          <Link to="/first" style={{textDecoration: "none",color:'white',fontWeight:'bold'}}><ListBox>Project 1</ListBox></Link>
+          <Link to="/second" style={{textDecoration: "none",color:'white',fontWeight:'bold'}}><ListBox>Project 2</ListBox></Link>
+          <Link to="/third" style={{textDecoration: "none",color:'white',fontWeight:'bold'}}><ListBox>Project 3</ListBox></Link>
+          <Link to="/fourth" style={{textDecoration: "none",color:'white',fontWeight:'bold'}}><ListBox>Project 4</ListBox></Link>
+        </Nav>
+        <Main>
+        <Routes>
+            <Route path="/" element={<FirstList/>}></Route>
+            <Route path="/portfolio" element={<FirstList/>}></Route>
+            <Route path="/first" element={<FirstList/>}></Route>
+            <Route path="/second" element={<FirstList/>}></Route>
+            <Route path="/third" element={<FirstList/>}></Route>
+            <Route path="/fourth" element={<FirstList/>}></Route>
+          </Routes>
+        </Main>
+      </HashRouter>
+    </Compoenet>
   );
 }
 export default App;
+
+const Compoenet = styled.div`
+  width:100vw;
+  height:100vh;
+  display: flex;
+`
+
+const Main = styled.div`
+  width:100vw;
+  height:100vh;
+  background-color:#404258;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  overflow-y: scroll;
+`
+
+const Nav = styled.div`
+  width:30vw;
+  height:100vh;
+  background-color: #404258;
+  //padding:10px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap:10px;
+`
+
+const ListBox = styled.div`
+  width:250px;
+  height:calc(70vh / 4);
+  background-color: ${(props)=>props.color|| "rgba(255,255,255,0.2)"};
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  border-radius: 0px 20px 20px 0px;
+
+  :hover{
+    background-color: tomato;
+  }
+  transition: background 0.5s ease-in-out;
+  
+  @media screen and (max-width: 1100px) {
+    width:200px;
+  }
+`
---
+++ src/component/Title.js
@@ -0,0 +1,26 @@
+import styled from "styled-components"
+
+const Title = () =>{
+  return(
+    <Component>
+      <div>
+        Jeong Yoon Hwan<br></br>
+        Portfolio
+      </div>
+    </Component>
+  )
+}
+
+export default Title;
+
+const Component = styled.div`
+  width:100%;
+  height:30%;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  color:white;
+  font-size: 2rem;
+  font-weight:bold;
+  text-align:center;
+`
